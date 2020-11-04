857ab09d717395d9a3554bcc43fb05d076545aea
Defne <defnesurujon@gmail.com>
2020-11-04 15:01:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'BFClust',
-    version = '0.1.26',
+    version = '0.1.26.1',
     description = 'Boundary Forest Clustering',
     url = 'https://github.com/dsurujon/BFClust-python',
     author = 'Defne Surujon',
